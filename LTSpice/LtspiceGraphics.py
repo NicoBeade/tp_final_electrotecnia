74781b61ea5252babad94d1fede012600acd2e5b
eloprestoripa <eloprestoripa@itba.edu.ar>
2023-06-03 00:54:55 -0300

    Graficos de LTSpice
    Graficos de carga y descarga tanto teoricos como simulados

--- LTSpice/LtspiceGraphics.py
+++ LTSpice/LtspiceGraphics.py
@@ -6,7 +6,7 @@ import os
 #------------Load the signals from the .raw file
 ruta_actual = os.path.realpath(__file__)
 ruta_carpeta = os.path.dirname(ruta_actual)
-raw_file = ruta_carpeta + '/Montecarlo_Completo/Simulacion_SPICE_Montecarlo_Completo.raw'
+raw_file = ruta_carpeta + '\\Simulacion_con_valores_Fijos\\Simulacion_SPICE_Valores_Fijos.raw'
 l = ltspice.Ltspice(raw_file) 
 # Make sure that the .raw file is located in the correct path
 l.parse()
@@ -18,8 +18,7 @@ Ts = 1/Fs
 
 #----------------------------Obtain basic signals
 time = l.get_time()
-time = (time - 1E-3)*1E+3                     
-
+time = (time) * 1E+3         
 Vcc = l.get_data('V(vcc)')
 Vlplus = l.get_data('V(vl+)')
 Vlminus = l.get_data('V(vl-)')
@@ -29,114 +28,48 @@ ic = l.get_data('I(C1)')
 id = l.get_data('I(V1)')
 #------------------------------------------------
 
+chargeTime = np.linspace(0, 200, 1000)
+dischargeTime = np.linspace(200, 400, 1000)
 
-currents = [ic]
-voltages = [Vl]
+#tension de carga de la bobina
+Vltc = - 0.19 * 0.45 * np.exp(-42.73 * (chargeTime -10) * 1E-3) * ( 42.73 * np.cos( 73.84 * (chargeTime -10) * 1E-3 + 2.79) + 73.84 * np.sin( 73.84 * (chargeTime -10) * 1E-3 + 2.79))
+#tension de descarga de la bobina
+Vltd = 0.19 * 0.45 * np.exp(-42.73 * (dischargeTime - 210) * 1E-3) * ( 42.73 * np.cos( 73.84 * (dischargeTime - 210) * 1E-3 + 2.79) + 73.84 * np.sin( 73.84 * (dischargeTime - 210) * 1E-3 + 2.79))
 
-num_signals = len(currents) + len(voltages)
+#Corriente de carga del capacitor
+ictc = - 7.38  * 300E-6 * np.exp(-42.73 * (chargeTime -10) * 1E-3) * ( 42.73 * np.cos( 73.84 * (chargeTime -10) * 1E-3 + 4.567) + 73.84 * np.sin( 73.84 * (chargeTime -10) * 1E-3 + 4.567))
+#Corriente de descarga del capacitor
+ictd = 7.38  * 300E-6 * np.exp(-42.73 * (dischargeTime - 210) * 1E-3) * ( 42.73 * np.cos( 73.84 * (dischargeTime - 210) * 1E-3 + 4.567) + 73.84 * np.sin( 73.84 * (dischargeTime - 210) * 1E-3 + 4.567))
 
-currents_labels = ["$i_C$"]
-voltages_labels = ["$V_L$"]
 
 #Set max and min Voltages and currents
-V_upper_limit = 13
-V_lower_limit = -13
+V_upper_limit = 5
+V_lower_limit = -5
 I_upper_limit = 0.5
 I_lower_limit = -0.5
 
 fig = plt.figure(figsize=(15, 15))
 fig.subplots_adjust(hspace=0.5)
 
-for i in range(len(currents)):
-    plt.subplot(2,1,i + 1)
+#Current plot
+plt.subplot(2, 1, 1)
+plt.title("$[A]$")       #title
+plt.xlabel("$Time\ [m seg]$")
+plt.xlim(0, Ts*1E+3)
+plt.ylim(I_lower_limit, I_upper_limit)
 
-    plt.title(currents_labels[i] + " $ [A]$")       #title
-    #axis
-    plt.xlabel("$Time\ [m seg]$")
-    plt.xlim(0, Ts*1E+3)
-    plt.ylim(I_lower_limit, I_upper_limit)
-    #plot function
-    plt.plot(time , currents[i], color = 'blue')
+plt.plot(chargeTime , ictc, color = (0,0, 0.5))
+plt.plot(dischargeTime , ictd, color = (0,0, 1))
+plt.plot(time , ic, linestyle = '--', color = (1, 1, 0))
 
-for i in range(len(voltages)):
+#Voltage plot
+plt.subplot(2, 1, 2)
+plt.title("$[V]$")
+plt.xlabel("$Time\ [m seg]$")
+plt.xlim(0, Ts*1E+3)
+plt.ylim(V_lower_limit, V_upper_limit)
 
-    plt.subplot(2,1,i + 1 + len(currents))
-
-    plt.title(voltages_labels[i] + " $ [V]$")       #title
-    #axis
-    plt.xlabel("$Time\ [m seg]$")
-    plt.xlim(0, Ts*1E+3)
-    plt.ylim(V_lower_limit, V_upper_limit)
-    plt.axhline(y= Vcc[100], linestyle='--', linewidth = 0.5, color='grey')
-    #plot function
-    plt.plot(time , voltages[i], color = 'red')
-
-
-
+plt.plot(chargeTime , Vltc, color = (0.3,0,0))
+plt.plot(dischargeTime , Vltd, color = (1,0,0))
+plt.plot(time , Vl, linestyle = '--', color = (0, 1, 1))
 plt.show()
-
-
-'''
-for i, ax in enumerate(axs.flatten()):
-    xticks_list = []
-    xticks_labels = []
-    yticks_list = []
-    yticks_labels = []
-
-    if i%2 == 0:    #Voltage signals are odd (0, 2, 4)
-        ax.plot(time, signals[i], color = 'red')
-        ax.set_title(labels[i] + " $ [V]$")
-
-        yticks_list.append(Vl)
-        
-        yticks_labels.append("$VL$")
-
-        ax.axhline(y= Vd_param, linestyle='--', linewidth = 0.5, color='grey')
-
-        ax.set_ylim(V_lower_limit, V_upper_limit)
-
-        ax.plot(time, signals[i], color = 'red')
-        
-    else:
-        ax.set_title(labels[i] + " $[A]$")
-        ax.set_ylim(I_lower_limit, I_upper_limit)
-
-        ax.plot(time, signals[i], color = 'blue')
-
-    if labels[i] == "$i_L$":
-        ax.plot(time, il, color = 'teal', linestyle='--')
-        ax.annotate("$i_o$", xy=(0, il.mean()), xytext=(-1.2, il.mean()),
-            arrowprops=dict(facecolor='black', arrowstyle='->'))
-
-    ax.set_xlabel("$Time\ [\mu seg]$")
-    ax.grid(False)
-    #Set the part we want to see
-    ax.set_xlim(0, N_periods*Ts*1E6)
-    
-    #Draw vertical lines at the points of switching
-    for i in range(int(N_periods)):
-        ax.axvline(x=(D*Ts + i*Ts)*1E6, linestyle='--', linewidth = 0.5,color='grey')
-        ax.axvline(x=(1 + i)*Ts*1E6, linestyle='--', linewidth = 0.5,color='grey')    
-
-        xticks_list.append((D*Ts + i*Ts)*1E6)
-        xticks_list.append((1 + i)*Ts*1E6)
-
-        if i == 0:
-            xticks_labels.append("$DTs$")
-            xticks_labels.append("$Ts$")
-        else:
-            xticks_labels.append("$(D" + "+" + str(i) + ")Ts$")
-            xticks_labels.append("$" + str(i+1) + "Ts$")
-
-        #Agrego el 0
-        ax.axhline(y= 0, linestyle='--', linewidth = 0.5, color='grey')
-        yticks_list.append(0)
-        yticks_labels.append("$0$")
-
-    ax.set_xticks(xticks_list)
-    ax.set_yticks(yticks_list)
-    ax.set_xticklabels(xticks_labels)
-    ax.set_yticklabels(yticks_labels)
-
-    ax.yaxis.set_label_coords(-0.01, 0.9)
-'''
