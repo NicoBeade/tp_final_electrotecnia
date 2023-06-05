98cac02dfd9edc3f0be37e98692740ce98cdde00
eloprestoripa <eloprestoripa@itba.edu.ar>
2023-06-05 17:28:40 -0300

    Input de señal de entrada

--- Python/src/mainwindow.py
+++ Python/src/mainwindow.py
@@ -10,10 +10,15 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setupUi(self)
+        #System input
         self.SystemOrder.currentIndexChanged.connect(self.changeSystemFrame)
         self.FirstOrderWidget.show()
         self.SecondOrderWidget.hide()
         self.SupOrderWidget.hide()
+        #Entry input
+        self.InputType.currentIndexChanged.connect(self.changeInputFrame)
+        self.PhaseLabel.show()
+        self.DutyCicleLabel.hide()
 
     def changeSystemFrame(self):
         order = self.SystemOrder.currentIndex()
@@ -30,3 +35,27 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             self.SecondOrderFilters.show()
         elif( order == 2 ):
             self.SupOrderWidget.show()
+
+    def changeInputFrame(self):
+        order = self.InputType.currentIndex()
+        if( order == 0):
+            self.EntryFrecInput.show()
+            self.EntryFrecLabel.show()
+            self.Phase_DutyCicleInput.show()
+            self.PhaseLabel.show()
+            self.DutyCicleLabel.hide()
+        if( order == 1):
+            self.EntryFrecLabel.hide()
+            self.EntryFrecInput.hide()
+            self.Phase_DutyCicleInput.hide()
+            self.PhaseLabel.hide()
+            self.DutyCicleLabel.hide()
+        if( order == 2 or order == 4):
+            self.EntryFrecLabel.show()
+            self.EntryFrecInput.show()
+            self.Phase_DutyCicleInput.show()
+            self.PhaseLabel.hide()
+            self.DutyCicleLabel.show()
+        
+            
+
